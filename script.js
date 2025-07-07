fd4fd71ef51aaa4f4bcd9a1dfe42a7ced8211dca
Tejashri Mandlik <161927572+tejashri4@users.noreply.github.com>
2025-07-07 05:12:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//Task 1 : Variable & console . //
+
+alert("Hello from external JavaScript file!");
+console.log("External JS is working!");
+console.log("Welcomeee");
+let name = "Tejuuuu";
+const birthYear = 2000;
+var city = "Pune";
+
+console.log(name);
+console.log(birthYear);
+console.log(city);
+
+let yourName = "Tejuu";
+let yourAge = 24;
+let favoriteColor = "Blue";
+
+console.log("My name is : " +yourName);
+console.log("My Age is : " +yourAge);
+console.log("My favorite Color: " +favoriteColor);
+
+alert("Welcome" +yourName + "! Let's start learining JavaScript!");
---
+++ script2.js
@@ -0,0 +1,22 @@
+//Task 2:Data Type & ypeof OPerator
+
+let name = "Teju";
+let age = 24;
+let isStudent = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof  isStudent);
+
+let yourname = "Tejuu";
+let yourage = 24;
+let isHappy = true;
+let phoneNumber;
+let emptyValue = null;
+
+console.log("Type of variables....");
+console.log( typeof yourname );
+console.log(typeof yourage);
+console.log(typeof isHappy);
+console.log(typeof phoneNumber);
+console.log(typeof emptyValue);
