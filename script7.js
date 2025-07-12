88f5034fb915d43250022f4099923262ec36b102
Tejashri Mandlik <161927572+tejashri4@users.noreply.github.com>
2025-07-12 23:50:22 +0530

    Array

---
+++ script7.js
@@ -0,0 +1,42 @@
+//Array task:
+//1.Print color in Array.
+let colors = ["Red" , "Blue" , "Green" , "Yellow" , "Purple"];
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[3]);
+console.log(colors[4]);
+console.log(colors[5]);
+
+//2.print 3 fruits number.
+let fruits = ["Apple", " Banna", "Mangooo"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+//3.Prints 2 even numbers
+let numbers = [2,4];
+console.log(numbers[0]);
+console.log(numbers[1]);
+
+//4.Add two numbers in array:
+let nums = [5,10];
+let sum = nums[0] + nums[1];
+console.log("Sum is : " +sum);
+
+//5.use push and pop
+let animals = ["Cat"];
+animals.push("Dog");
+console.log(animals[0]);
+
+animals.pop();
+console.log(animals);
+
+//6.Print biggest number from 2 values
+let marks = [60,90];
+if(marks[0] > marks[1])
+{
+    console.log(marks[0]);
+}
+else{
+    console.log(marks[1]);
+}
