3762c0d723df629756f8c6e9eac7e6e239d86930
Tejashri Mandlik <161927572+tejashri4@users.noreply.github.com>
2025-07-14 22:04:44 +0530

    Add files via upload

---
+++ script9.js
@@ -0,0 +1,34 @@
+//Task 9 : JavaScript Condition + Basic DOM
+//PART 1: if/else and switch
+//1.Simple if/else
+let age = 18;
+
+if(age >= 18 )
+{
+    console.log("You can vote.");
+}
+else{
+    console.log("you cannot vote.");
+}
+
+//2.Use switch statement
+let day = 4;
+switch(day)
+{
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thuesday");
+        break;
+
+    default :
+        console.log("Other day");
+}
